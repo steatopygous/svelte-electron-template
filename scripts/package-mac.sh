187af77fc5739acb4d215f81171865bb67ccf5a2
Harry Ohlsen <hohlsen@iinet.net.au>
2019-07-10 12:16:44 +1000

    Put the command on one line

--- scripts/package-mac.sh
+++ scripts/package-mac.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-electron-packager . --overwrite --platform=darwin --arch=x64 \
-   --icon=assets/icons/macos/icon.icns --prune=true --out=release-builds
+npx electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/macos/icon.icns --prune=true --out=release-builds
